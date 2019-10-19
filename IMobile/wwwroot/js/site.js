9622e1f331552bb1e23d629af81594cfc391c4b6
orow1993 <orow1993@gmail.com>
2019-10-19 21:43:09 +0300

    add CSS

--- IMobile/wwwroot/js/site.js
+++ IMobile/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function updateTransition() {
+    var el = document.querySelector("div.box");
+
+    if (el) {
+        el.className = "box1";
+    } else {
+        el = document.querySelector("div.box1");
+        el.className = "box";
+    }
+
+    return el;
+}
+
+var intervalID = window.setInterval(updateTransition, 7000);
