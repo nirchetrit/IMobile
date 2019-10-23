38d9288496f3d391f07cdb4ee2da36995fa10beb
orow1993 <orow1993@gmail.com>
2019-10-23 17:58:52 +0300

    devices

--- IMobile/wwwroot/js/site.js
+++ IMobile/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#clickme").click(function () {
+    $("#book").slideUp("slow", function () {
+        // Animation complete.
+    });
+});
